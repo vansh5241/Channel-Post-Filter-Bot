3f5172491bdde332dd3c9e4d19741a5af82e75e4
vansh5241 <145540382+vansh5241@users.noreply.github.com>
2023-09-20 00:55:36 -0700

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 21956488
 API_HASH     = "812529f879f06436925c7d62eb49f5d1"
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "@CyNiTeBaCkUp"
+BOT_TOKEN    = "6496595280:AAHUKsD94eYYqcfj_jsOnhxmUOyAI09rQdc"
+SESSION      = "BQFPB4gAVetaaeQzdPJRaCwgqVD-KNkMkJ59egCvywg6_qiZqU0O2najP3apnOEcgFIbBlk74tZabvvWqNHYNicbOO7Vmc7RBdHjdfVTtgyMwgvIwbop2QyZ3S0kcCgGQUBFwssdmnQwAoYhRuAHT_Cs5YYud4oywuuNOSIHBO8f1kK-oKcBYPbK2sZgsDe10kE2jcKYo2RMHM4XuaQJVzGfRHj1s06uO0Om_k6G0bDVmwif5tXNMjzck_D-Oik4hTD7azMxqSfKVd-43xKO18z79gs6qlen0ywPE3LQ5NFwdoyuSTNzO_2EFKWsP9ce7-PD-vMHPk1LyKDBdHcWezaEODDsAAAAAAB4ai0IAA "
+DATABASE_URI = "mongodb+srv://vayadav163:9837626609@cluster0.gn6gspr.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001876702744
+ADMIN        = 2020224264
+CHANNEL      = "@vip_bro10"
